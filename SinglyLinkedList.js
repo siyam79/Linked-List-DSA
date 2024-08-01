b6d3c41beb402dae76686e635ef8c9f789280182
siyam79 <siyamahmed3827@gmail.com>
2024-08-01 09:57:59 +0600

    Linked List Node push implements

---
+++ SinglyLinkedList.js
@@ -0,0 +1,34 @@
+class SinglyLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    isEmpty() {
+        return this.length === 0
+    }
+
+    push(value) {
+        let newNode = {
+            value: value,
+            next: null
+        }
+
+        if (this.isEmpty()) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+    }
+}
+let list = new SinglyLinkedList()
+
+list.push(5)
+list.push(10)
+list.push(15)
+list.push(20)
+console.log(list);
