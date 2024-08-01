6230d30e95ae8f7eb76c1450aad181bfe23dcaa3
siyam79 <siyamahmed3827@gmail.com>
2024-08-01 10:23:02 +0600

    Linked List Node pop implements

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -24,6 +24,38 @@ class SinglyLinkedList {
         }
         this.length++
     }
+
+    pop(value) {
+        if (!this.head) {
+            return null
+        }
+
+        if (this.length === 1) {
+            let removeNode = this.head
+            this.head = null
+            this.tail = null
+            this.length = 0
+            return removeNode
+        }
+
+        let currentNode = this.head
+        let lastNode = this.tail
+        let newLastNode
+        while (currentNode) {
+
+            if (currentNode.next == this.tail) {
+                newLastNode = currentNode
+                break
+            }
+            currentNode = currentNode.next
+        }
+
+        newLastNode.next = null
+        this.tail = newLastNode
+        this.length--
+
+        return lastNode
+    }
 }
 let list = new SinglyLinkedList()
 
@@ -31,4 +63,5 @@ list.push(5)
 list.push(10)
 list.push(15)
 list.push(20)
+list.pop()
 console.log(list);
